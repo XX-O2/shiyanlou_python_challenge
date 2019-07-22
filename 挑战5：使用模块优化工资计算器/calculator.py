4082d285bbb37a40393e66390f1bdfcac7408e47
Mola <18842653036@163.com>
2019-07-22 10:39:11 +0800

    挑战

--- 挑战5：使用模块优化工资计算器/calculator.py
+++ 挑战5：使用模块优化工资计算器/calculator.py
@@ -1,58 +1,76 @@
-#!/usr/bin/env pytho3
-
-import time
-from datetime import datetime,date
+from multiprocessing import Process,Queue
 import sys
 import csv
-import json
 import getopt
+from datetime import datetime,date
 from configparser import ConfigParser
 
-def shebao_calculate(num,total,min,max):
-    num = float(num)
-    if num<min:
+queue1 = Queue()
+queue2 = Queue()
+
+#global min
+#global max
+#global total
+
+def rate(before_tax_salary):
+    if before_tax_salary<min:
         num_1 = min
-    elif num>max:
+    elif before_tax_salary>max:
         num_1 = max
     else:
-        num_1 = num
-    shebao_money = num_1 * total
-    should_tax = num-shebao_money-5000
+        num_1 = before_tax_salary
+    shebao_money = num_1*total
+    should_tax = before_tax_salary-shebao_money-5000
     if should_tax<=0:
-        tax = 0
+        tax=0
     elif 0<should_tax<=3000:
-        tax = should_tax*0.03
+        tax=should_tax*0.03
     elif 3000<should_tax<=12000:
-        tax = should_tax*0.1-210
+        tax=should_tax*0.1-210
     elif 12000<should_tax<=25000:
-        tax = should_tax*0.2-1410
+        tax=should_tax*0.2-1410
     elif 25000<should_tax<=35000:
-        tax = should_tax*0.25-2660
+        tax=should_tax*0.25-2660
     elif 35000<should_tax<=55000:
-        tax = should_tax*0.3-4410
+        tax=should_tax*0.3-4410
     elif 55000<should_tax<=80000:
-        tax = should_tax*0.35-7160
+        tax=should_tax*0.35-7160
     else:
-        tax = should_tax*0.45-15160
-    gongzi = num-shebao_money-tax
-    return shebao_money,tax,gongzi
+        tax=should_tax*0.45-15160
+    salary = before_tax_salary-shebao_money-tax
+    return shebao_money,tax,salary
 
-def calculate(user,total,min,max):
-    user_info = []
-    id = user[0]
-    num = user[1]
-    a, b, c = shebao_calculate(num,total,min,max)
-    user_info.append(int(id))
-    user_info.append(int(num))
-    user_info.append('{:.2f}'.format(float(a)))
-    user_info.append('{:.2f}'.format(float(b)))
-    user_info.append('{:.2f}'.format(float(c)))
-    user_info.append(datetime.now().date())
-    return user_info
+def q1(queue1,user_data):
+    data=[]
+    ID = user_data[0]
+    before_tax_salary = user_data[1]
+    data.append(ID)
+    data.append(before_tax_salary)
+    queue1.put(data)
 
+def q2(queue1,queue2):
+    data = queue1.get()
+    newdata=[]
+    ID = data[0]
+    before_tax_salary=float(data[1])
+    cal_time = datetime.now().time()
+    hours,minutes,seconds = [cal_time.hour,cal_time.minute,cal_time.second]
+    shebao_money,tax,salary = rate(before_tax_salary)
+    newdata.append(ID)
+    newdata.append(int(before_tax_salary))
+    newdata.append('{:.2f}'.format(shebao_money))
+    newdata.append('{:.2f}'.format(tax))
+    newdata.append('{:.2f}'.format(salary))
+    newdata.append(str(datetime.now())[:-7])
+    queue2.put(newdata)
 
-if __name__ == "__main__":
-    # ??????
+def q3(queue2,gongzifile):
+    newdata=queue2.get()
+    with open(gongzifile,"a+") as f2:
+        writer = csv.writer(f2)
+        writer.writerow(newdata)
+
+def main():
 
     opts,args = getopt.getopt(sys.argv[1:],'hC:c:d:o:',['help'])
     options = dict(opts)
@@ -67,23 +85,24 @@ if __name__ == "__main__":
         the_config = dict(config.items(the_city))
     else:
         the_config = dict(config['DEFAULT'])
+    global total
     total = 0
     for key, value in the_config.items():
         if key == "JiShuL".lower():
+            global min
             min = float(value)
         elif key == "JiShuH".lower():
+            global max
             max = float(value)
         else:
             total += float(value)
 
-    # ????????
-    with open(userfile, 'r') as f1:
-        users = list(csv.reader(f1))
-        users_info = []
-        for user in users:
-            user_info = calculate(user,total,min,max)
-            users_info.append(user_info)
+    with open(userfile,'r') as f4:
+        users = list(csv.reader(f4))
+        for user_data in users:
+            Process(target=q1,args=(queue1,user_data)).start()
+            Process(target=q2,args=(queue1,queue2)).start()
+            Process(target=q3,args=(queue2,gongzifile)).start()
 
-    with open(gongzifile,'w') as f3:
-        csv.writer(f3).writerows(users_info)
-
+if __name__ == "__main__":
+    main()
